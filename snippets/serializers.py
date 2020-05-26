a979e39b49c2fda07b6e3b022882be010d912396
Lanting Chiang <lantingchiang@gmail.com>
2020-05-26 02:12:50 -0400

    part 1

--- snippets/serializers.py
+++ snippets/serializers.py
@@ -2,7 +2,9 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 
+"""
 class SnippetSerializer(serializers.Serializer):
+    # DEFINE FIELDS THAT GET SERIALIZED
     id = serializers.IntegerField(read_only=True)
     # allow_blank is only used for CharField, makes "" a valid entry
     title = serializers.CharField(required=False, allow_blank=True)
@@ -11,3 +13,29 @@ class SnippetSerializer(serializers.Serializer):
     language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default="python")
     style = serializers.ChoiceField(choices=STYLE_CHOICES, default="friendly")
 
+    def create(self, validated_data):
+        # Creates and returns new Snippet instance from validated data
+        # ** unpackes/flattens the keyword argument (dictionary) to its values
+        # Creates new instance but doesn't save it
+        return Snippet.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        # Update and return existing snippet instance from validated data
+        # validated_data is ordered dict; .get() gets the value for given key
+        # .get() returns instance.title is validated_data['title'] doesn't exist
+        instance.title = validated_data.get('title', instance.title)
+        instance.code = validated_data.get('code', instance.code)
+        instance.linenos = validated_data.get('linenos', instance.linenos)
+        instance.language = validated_data.get('language', instance.language)
+        instance.style = validated_data.get('style', instance.style)
+        # updates existing instance
+        instance.save()
+        return instance
+"""
+
+
+class SnippetSerializer(serializers.ModelSerializer):
+    # has default create() & update() methods
+    class Meta:
+        model = Snippet
+        fields = ["id", "title", "code", "linenos", "language", "style"]
--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,64 @@
-from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.parsers import JSONParser
+from .models import Snippet
+from .serializers import SnippetSerializer
 
-# Create your views here.
+
+@csrf_exempt
+def snippet_list(request):
+    """
+    Lists all existing snippets or creates a new one
+    """
+    if request.method == "GET":
+        snippets = Snippet.objects.all()
+        # INPUT: query set instead of single instance --> gives list of dicts
+        serializer = SnippetSerializer(snippets, many=True)
+        # first parameter should be dictionary; setting safe=False allows any json serializable object
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == "POST":
+        # JSONParser parses JSON request content
+        data = JSONParser().parse(request)
+        # INPUT: json data (?)
+        serializer = SnippetSerializer(data=data)
+
+        if serializer.is_valid():
+            # returns snippet object by create() method
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def snippet_detail(request, pk):
+    """
+    Retrieve, update or delete code snippet
+    """
+    # find the object of interest with pk first
+    try:
+        # model.pk contains primary key value (whether it's the default id or provided)
+        snippet = Snippet.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return HttpResponse(status=404)
+
+    # RETRIEVE case
+    if request.method == "GET":
+        # INPUT: snippet instance
+        serializer = SnippetSerializer(snippet)
+        return JsonResponse(serializer.data)
+
+    # UPDATE case
+    elif request.method == "PUT":
+        data = JSONParser().parse(request)
+        # INPUT: snippet instance + data
+        serializer = SnippetSerializer(snippet, data=data)
+        if serializer.is_valid():
+            # calls update() method in serializer with given instance & data
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == "DELETE":
+        snippet.delete()
+        return HttpResponse(status=204)
